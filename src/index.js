dc90ceb30eb29f11508144715f61d00fbe3aabe8
IsmaelTerreno <elismakof2002@gmail.com>
2024-12-06 22:56:31 -0300

    Add Marketplace contract JSON artifact
    This commit introduces the JSON artifact for the Marketplace contract. The artifact includes the contract's ABI, errors, events, and functions to support marketplace operations such as listing and purchasing items, managing ownership, and handling signatures. This addition is essential for interfacing with the Marketplace smart contract in various development and testing environments.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {loadWeb3App} from "./lib/web3-utils/client";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +11,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+loadWeb3App(() => {
+}).then(r => console.log('Web3 loaded successfully'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
