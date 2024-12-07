da1fa8637638b0a9f628e96e3e0b8c49f9253fbc
IsmaelTerreno <elismakof2002@gmail.com>
2024-12-07 16:11:36 -0300

    Enhance UI styling for Crypto Marketplace app
    Added new CSS classes for improved styling, including background colors, text sizes, and padding. Updated the App.jsx component to use these styles, enhancing the visual appeal and responsive design of the "Crypto Marketplace" page. The changes provide a more polished look, improving user engagement when connecting wallets.

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import { useWalletConnect } from "./hooks/useWalletConnect";
 const App = () => {
   const { connectionRequest, isConnectedWallet, account } = useWalletConnect();
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-amber-800 text-center">Wallet Connect</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+      <h1 className="text-amber-800 text-center text-5xl pb-4">
+        Crypto Marketplace
+      </h1>
       {!isConnectedWallet && (
         <button
           onClick={connectionRequest}
-          className="px-4 py-2 mt-4 text-sm font-semibold leading-normal text-gray-500 transition duration-300 ease-in-out border border-transparent rounded hover:text-gray-400 hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
+          className="px-6 py-3 mt-4 text-md font-bold text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition ease-in-out duration-300"
         >
           Connect wallet
         </button>
       )}
       {isConnectedWallet && (
         <div className="mt-4 text-center">
-          <p>Connected wallet</p>
+          <p className="text-center">Connected wallet</p>
           <p>Account: {account}</p>
         </div>
       )}
